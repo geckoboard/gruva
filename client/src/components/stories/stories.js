ba2aee08be771a91b4bc9e220155f0023585d61f
Hanna Hollstrom <hanna@geckoboard.com>
2020-06-25 16:21:28 +0100

    Section for in progress cards

--- client/src/components/stories/stories.js
+++ client/src/components/stories/stories.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import { orderBy } from 'lodash';
 import Story from '../story';
 import styles from './stories.css';
@@ -13,6 +14,25 @@ const sortStories = stories => {
   );
 };
 
+const splitStories = stories => {
+  return stories.reduce(
+    (acc, story) => {
+      const allStories = { ...acc };
+      if (story.started && !story.completed) {
+        allStories.started.push(story);
+      } else {
+        allStories.rest.push(story);
+      }
+
+      return allStories;
+    },
+    {
+      started: [],
+      rest: [],
+    },
+  );
+};
+
 class Stories extends Component {
   render() {
     const { isEpicDone, isLoading, stories } = this.props;
@@ -21,18 +41,34 @@ class Stories extends Component {
       return <div>No stories for this epic</div>;
     }
 
-    const sortedStories = sortStories(stories);
+    const storiesByType = splitStories(stories);
+    const startedStories = sortStories(storiesByType.started);
+    const restStories = sortStories(storiesByType.rest);
 
     return (
       <div className={styles.stories}>
-        {sortedStories.map(story => (
-          <Story
-            key={story.id}
-            story={story}
-            isEpicDone={isEpicDone}
-            isLoadingStories={isLoading}
-          />
-        ))}
+        {startedStories.length > 0 && (
+          <div className={classnames(styles.storiesSection, styles.started)}>
+            {startedStories.map(story => (
+              <Story
+                key={story.id}
+                story={story}
+                isEpicDone={isEpicDone}
+                isLoadingStories={isLoading}
+              />
+            ))}
+          </div>
+        )}
+        <div className={styles.storiesSection}>
+          {restStories.map(story => (
+            <Story
+              key={story.id}
+              story={story}
+              isEpicDone={isEpicDone}
+              isLoadingStories={isLoading}
+            />
+          ))}
+        </div>
       </div>
     );
   }
